e3d305857abe4118c976233ab33d4e74f5d5e0e5
Adithya <54103522+adithyaan-creator@users.noreply.github.com>
2020-10-08 11:52:57 +0530

    Update luluScraper.py

--- luluScraper.py
+++ luluScraper.py
@@ -1,10 +1,10 @@
-import scrapy
-
 class ProductDetails(scrapy.Item):
     itemName = scrapy.Field()
     brandName = scrapy.Field()
     priceAED = scrapy.Field()
     stockAvailability = scrapy.Field()
+    prodDescr = scrapy.Field()
+    imageLinks = scrapy.Field()
 
 class LuluSprider(scrapy.Spider):
 
@@ -19,7 +19,7 @@ class LuluSprider(scrapy.Spider):
    #list of allowed domains
     allowed_domains = ['www.luluhypermarket.com']
    #starting url
-    start_urls = ['https://www.luluhypermarket.com/en-ae/fresh-food-grocery/c/HY00216086?q=%3Arelevance&page={pageNumber}'.format(pageNumber=page) for page in range(1,40)]
+    start_urls = ['https://www.luluhypermarket.com/en-ae/fresh-food-grocery/c/HY00216086?q=%3Arelevance&page={pageNumber}'.format(pageNumber=page) for page in range(1,3)]
    
 
     def scrape_product(self, response):
@@ -27,7 +27,9 @@ class LuluSprider(scrapy.Spider):
         brand = response.css('strong label::text').extract()
         stock = response.css('span.stock-available::text').extract()
         price = response.css('.prod-price span::text').extract()
-        yield ProductDetails(itemName = item, brandName = brand, priceAED = price, stockAvailability = stock)
+        productDescription = response.css('.description ul li::text').extract()
+        image_url = response.css('.item img.lazyOwl::attr(src)').extract()
+        yield ProductDetails(itemName = item, brandName = brand, priceAED = price, stockAvailability = stock, prodDescr = productDescription, imageLinks = image_url)
    
     def parse(self, response):
 
